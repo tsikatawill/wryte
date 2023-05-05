97fc41a0a71f23217114e6f4d30461eea40a22bd
William M. Tsikata <tsikatawill@gmail.com>
2023-05-05 01:40:59 +0000

    add routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,7 @@
+import { routeT } from "../types";
+
+export const ROUTES: routeT[] = [
+  { name: "Blog Posts", href: "/posts" },
+  { name: "Contact", href: "/posts" },
+  { name: "Newsletter", href: "/newsletter" },
+];
