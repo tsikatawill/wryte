3e03751179c1d9d11682843b262f26e2f68a5859
William M. Tsikata <tsikatawill@gmail.com>
2023-05-06 02:08:21 +0000

    export types

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,29 @@ export type routeT = {
   name: string;
   href: string;
 };
+
+export type postCategoryT =
+  | "Frontend"
+  | "Backend"
+  | "Product Management"
+  | "UI/UX";
+
+export type postT = {
+  id: string;
+  title: string;
+  author: string;
+  image?: string;
+  fullText: string;
+  category: postCategoryT;
+  // dateCreated: Date;
+  dateCreated: string;
+};
+
+export type filterFormDataT = {
+  category: "All" | postCategoryT;
+  author: string;
+  title: string;
+};
+
+export type filterByT = "author" | "category" | "title";
+export type filterValueT = string | postCategoryT;
