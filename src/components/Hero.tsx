c4fa736ecb7894bda97b0b33ae14ddb9a0b93e13
William M. Tsikata <tsikatawill@gmail.com>
2023-05-06 11:47:16 +0000

    handle responsiveness of pen icon + text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,35 @@
 import { AnimatedPenHead, Container } from ".";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const scrollRef = useRef(null);
   return (
-    <section className="hero">
+    <section ref={scrollRef} className="hero">
       <Container>
         <div className="max-w-xl text-center pt-12 pb-24 mx-auto">
-          <div className="icon-wrapper  mx-auto min-h-[80vh]">
-            <AnimatedPenHead />
+          <div className="icon-wrapper mx-auto mb-7">
+            <AnimatedPenHead parentRef={scrollRef} />
           </div>
 
-          <div className="min-h-screen grid place-content-center">
-            <h1 className="font-bold text-7xl leading-[7rem]">Wryte</h1>
-            <p className="text-3xl font-semibold leading-10 mt-5">
+          <motion.div
+            whileInView={{
+              y: [100, 0],
+              opacity: [0, 1],
+              offset: ["top center"],
+              transition: { ease: "easeIn", delay: 0.5, duration: 1 },
+            }}
+            viewport={{ once: true }}
+            className="py-10 grid place-content-center mb-10"
+          >
+            <h1 className="font-bold text-5xl md:text-7xl md:leading-[7rem]">
+              Wryte
+            </h1>
+            <p className="text-xl md:text-3xl font-semibold md:leading-10 mt-5">
               Insightful Articles Curated By A Wonderful Community For Everyone
               In The Tech Space
             </p>
-          </div>
+          </motion.div>
         </div>
       </Container>
     </section>
