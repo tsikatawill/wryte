72cbf10ca5171a2278dc67793f8b9ade9f741d2f
William M. Tsikata <tsikatawill@gmail.com>
2023-05-06 01:39:36 +0000

    fix duplicate route paths

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,6 +2,6 @@ import { routeT } from "../types";
 
 export const ROUTES: routeT[] = [
   { name: "Blog Posts", href: "/posts" },
-  { name: "Contact", href: "/posts" },
+  { name: "Contact", href: "/contact" },
   { name: "Newsletter", href: "/newsletter" },
 ];
