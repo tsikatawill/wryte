6b0a34887f3b2df6b705f1687a7d63254277bc62
William M. Tsikata <tsikatawill@gmail.com>
2023-05-06 01:37:08 +0000

    add posts container

---
+++ src/components/PostsContainer.tsx
@@ -0,0 +1,29 @@
+import { Container, PostCard } from ".";
+import { FilterRow } from "./FilterRow";
+import { motion } from "framer-motion";
+import { useFilteredPosts } from "../hooks/useFilteredPosts";
+
+export const PostsContainer = () => {
+  const { filterPosts, filteredPosts } = useFilteredPosts();
+
+  return (
+    <section className="py-10 md:py-20">
+      <Container>
+        <FilterRow handleFilter={filterPosts} />
+        <motion.div layout className="flex flex-wrap gap-10 justify-center">
+          {filteredPosts.length > 0 ? (
+            filteredPosts.map((post) => (
+              <PostCard layoutId={post.id} {...post} key={post.id} />
+            ))
+          ) : (
+            <div className="my-10">
+              <p className="text-red-500 font-semibold">
+                No posts match your filter results
+              </p>
+            </div>
+          )}
+        </motion.div>
+      </Container>
+    </section>
+  );
+};
