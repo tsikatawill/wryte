f0cdb3507741170f3567d106b2d7c5d16c6ebce1
William M. Tsikata <tsikatawill@gmail.com>
2023-05-07 16:20:46 +0000

    handle title font size for different devices

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -4,8 +4,8 @@ type Props = { title: string; description?: string };
 
 export const SectionHeader: FC<Props> = ({ title, description }) => {
   return (
-    <div className="mt-5">
-      <h1 className="mt-10 text-center text-3xl font-bold">{title}</h1>
+    <div className="mt-5 md:mt-10">
+      <h1 className="text-center text-3xl font-bold">{title}</h1>
 
       {description && <p>{description}</p>}
     </div>
