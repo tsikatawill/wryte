9d918db22f4fea4cca7dcc0edfe29cdbd938586a
William M. Tsikata <tsikatawill@gmail.com>
2023-05-07 16:21:23 +0000

    add route for create post

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,6 +2,7 @@ import { routeT } from "../types";
 
 export const ROUTES: routeT[] = [
   { name: "Blog Posts", href: "/posts" },
+  { name: "Create Post", href: "/create-post" },
   { name: "Contact", href: "/contact" },
   { name: "Newsletter", href: "/newsletter" },
 ];
