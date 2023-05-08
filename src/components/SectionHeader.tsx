ef57326a6c06511755a954c53a955f0b16defa67
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 02:00:40 +0000

    create section header

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -3,11 +3,19 @@ import { FC } from "react";
 type Props = { title: string; description?: string };
 
 export const SectionHeader: FC<Props> = ({ title, description }) => {
+  const titleArray = title.split(" ");
+  const sliced = titleArray.slice(0, titleArray.length - 1);
+
   return (
     <div className="mt-5 md:mt-10">
-      <h1 className="text-center text-3xl font-bold">{title}</h1>
+      <h2 className="text-center text-3xl font-bold">
+        <span>{sliced.join(" ")}</span>
+        <span className="text-blue-500"> {title.split(" ").slice(-1)}</span>
+      </h2>
 
-      {description && <p>{description}</p>}
+      {description && (
+        <p className="max-w-xl mx-auto text-center mt-2">{description}</p>
+      )}
     </div>
   );
 };
