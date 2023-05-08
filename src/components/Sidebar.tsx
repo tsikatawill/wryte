84ad94785b16ab6abd22f59f7c8d41499888a941
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 02:01:02 +0000

    add user button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { FC, ReactNode } from "react";
 import { SidebarBgAnim, SidebarMenuAnim } from "../lib/animations";
-import { motion } from "framer-motion";
 import { AiFillCloseCircle } from "react-icons/ai";
+import { UserButton } from ".";
+import { motion } from "framer-motion";
+import { useUser } from "../hooks/useUser";
 
 type Props = {
   show: boolean;
@@ -10,6 +12,8 @@ type Props = {
 };
 
 export const Sidebar: FC<Props> = ({ show, handleClose, children }) => {
+  const { user } = useUser();
+
   return (
     <motion.div
       className="fixed inset-0 bg-slate-950 bg-opacity-30 backdrop-blur-sm z-40"
@@ -20,7 +24,7 @@ export const Sidebar: FC<Props> = ({ show, handleClose, children }) => {
       <motion.aside
         onClick={(e) => e.stopPropagation()}
         variants={SidebarMenuAnim}
-        className="bg-white z-50 text-black w-72 dark:bg-slate-800 dark:text-white absolute right-0 h-screen overflow-y-scroll scrollbar-hidden p-2 sm:p-5"
+        className="bg-white z-50 text-black w-72 sm:w-80 dark:bg-slate-800 dark:text-white absolute right-0 h-screen overflow-scroll scrollbar-hidden p-2 sm:p-5"
       >
         <div className="w-full relative">
           <div
@@ -31,7 +35,11 @@ export const Sidebar: FC<Props> = ({ show, handleClose, children }) => {
           </div>
         </div>
 
-        {children}
+        <div className="flex justify-between gap-10 h-full flex-col">
+          {children}
+
+          {user && <UserButton user={user} type="wide" />}
+        </div>
       </motion.aside>
     </motion.div>
   );
