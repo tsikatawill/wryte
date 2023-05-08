7f416bcf3687dfdba4f01b7339f9ebf045570111
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 03:11:40 +0000

    wrap footer content in container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,21 @@
+import { Container } from ".";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="py-10 bg-black">
-      <p className="text-center">
-        Developed with ❤️‍🔥 by{" "}
-        <Link
-          to="https://twitter.com/dev_willman"
-          target="_blank"
-          className="text-blue-500"
-        >
-          William M. Tsikata
-        </Link>
-      </p>
+    <footer className="py-5 sm:py-10 text-white bg-black">
+      <Container>
+        <p className="text-center">
+          Developed with ❤️‍🔥 by{" "}
+          <Link
+            to="https://twitter.com/dev_willman"
+            target="_blank"
+            className="text-blue-500"
+          >
+            William M. Tsikata
+          </Link>
+        </p>
+      </Container>
     </footer>
   );
 };
