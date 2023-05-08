1e6d784dc082b414308f766534249e1184e26951
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 01:49:53 +0000

    add user button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,22 @@
-import { Container, Sidebar, ThemeToggle } from ".";
-import { Link, NavLink } from "react-router-dom";
+import {
+  Container,
+  NavMenuLink,
+  Sidebar,
+  SidebarLink,
+  ThemeToggle,
+  UserButton,
+} from ".";
 import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import Logo from "/images/wryte-light.svg";
 import { ROUTES } from "../lib/routes";
 import { useState } from "react";
+import { useUser } from "../hooks/useUser";
 
 export const Navbar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const { user } = useUser();
+
   return (
     <nav>
       <Container>
@@ -16,51 +26,39 @@ export const Navbar = () => {
           </Link>
 
           <ul className="nav-menu hidden gap-2 items-center md:flex ">
-            {ROUTES.map(({ href, name }) => (
-              <NavLink
-                to={href}
-                key={href}
-                title={name}
-                className={({ isActive }) => {
-                  return isActive
-                    ? `border-b border-black font-bold  dark:border-white dark:hover:bg-slate-950 hover:bg-slate-300 p-2 duration-300`
-                    : `dark:hover:bg-slate-950 hover:bg-opacity-40 font-semibold hover:bg-slate-200 border-transparent p-2 duration-300`;
-                }}
-              >
-                {name}
-              </NavLink>
-            ))}
+            {ROUTES.map((route) => {
+              if (user && route.name !== "Login")
+                return <NavMenuLink key={route.href} route={route} />;
+
+              if (!user && !route.protectedRoute)
+                return <NavMenuLink key={route.href} route={route} />;
+            })}
           </ul>
 
-          <div className="flex items-center gap-2 hover:scale-105 duration-75">
+          <div className="flex items-center gap-2">
             <button
-              className="md:hidden block"
+              className="md:hidden block hover:scale-110 duration-75"
               onClick={() => setShowSidebar(!showSidebar)}
             >
               <FaBars size={20} className="rotate-180" />
             </button>
 
             <ThemeToggle />
+
+            {user && <UserButton user={user} />}
           </div>
         </div>
       </Container>
 
       <Sidebar show={showSidebar} handleClose={() => setShowSidebar(false)}>
         <div className="flex flex-col gap-4 text-xl">
-          {ROUTES.map(({ href, name }) => (
-            <NavLink
-              to={href}
-              key={href}
-              title={name}
-              className={({ isActive }) => {
-                return isActive
-                  ? `border-l border-white font-bold dark:hover:bg-slate-900 bg-slate-200 dark:bg-slate-900  hover:bg-slate-300 p-2 hover:pl-3 duration-300`
-                  : `dark:hover:bg-slate-900 font-semibold hover:bg-slate-200 p-2 hover:pl-3 duration-300`;
-              }}
-            >
-              {name}
-            </NavLink>
-          ))}
+          {ROUTES.map((route) => {
+            if (user && route.name !== "Login")
+              return <SidebarLink key={route.href} route={route} />;
+
+            if (!user && !route.protectedRoute)
+              return <SidebarLink key={route.href} route={route} />;
+          })}
         </div>
       </Sidebar>
     </nav>
