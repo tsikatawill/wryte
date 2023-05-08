6b0b0e262699672b330f3d3243739d03f02d1640
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 01:59:16 +0000

    add edit and delete functionality

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,19 +1,28 @@
+import { FaPen, FaTrash } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import { MotionProps, motion } from "framer-motion";
 import { FC } from "react";
-import { Link } from "react-router-dom";
 import { PostCategoryIcon } from ".";
 import { postT } from "../types";
+import { toast } from "react-toastify";
+import { useUser } from "../hooks/useUser";
+import { usePosts } from "../hooks/usePosts";
 
 type Props = postT & MotionProps;
 
 export const PostCard: FC<Props> = ({
   title,
   author,
+  id,
   category,
   image,
   dateCreated,
   ...rest
 }) => {
+  const { user } = useUser();
+  const { deletePost } = usePosts();
+  const navigate = useNavigate();
+
   return (
     <motion.div
       exit={{ y: 200 }}
@@ -24,31 +33,51 @@ export const PostCard: FC<Props> = ({
       className="post-card bg-white shadow-md shadow-[rgba(0,0,0,0.25)] dark:bg-slate-700 dark:text-white w-full md:w-[21rem] lg:w-[23rem] max-w-sm"
       {...rest}
     >
-      <div className="image-wrapper max-w-sm w-full md:w-[21rem] lg:w-[23rem] sm:h-[21rem] lg:h-[23rem] overflow-hidden">
+      <div className="image-wrapper max-w-sm dark:bg-slate-950 bg-slate-200 w-full md:w-[21rem] lg:w-[23rem] sm:h-[21rem] lg:h-[23rem] overflow-hidden">
         <motion.img
           whileHover={{
             scale: 1.2,
             transition: { duration: 1, ease: "easeOut" },
           }}
           className="w-full h-full object-cover"
-          src={image}
+          src={image ? image : "https://picsum.photos/400"}
           alt={title}
         />
       </div>
 
-      <div className="text-content grid place-content-center h-60 relative px-2 sm:px-5">
+      <div className="text-content grid place-content-center relative py-5 sm:py-8 p-2 sm:p-5">
         <div className="absolute -top-5 left-1/2 -translate-x-1/2">
           <PostCategoryIcon category={category} />
         </div>
 
-        <div className="space-y-2 sm:space-y-5 text-center">
+        <div className="gap-2 flex flex-col items-center">
           <p className="text-slate-600 dark:text-slate-200">{author}</p>
 
-          <h3 className="font-bold capitalize text-2xl sm:text-3xl text-black dark:text-slate-100 line-clamp-3">
+          <h3 className="font-bold text-center capitalize text-xl sm:text-2xl text-black dark:text-slate-100 line-clamp-3">
             <Link to={`/posts/${title}`}>{title}</Link>
           </h3>
 
           <p className="text-slate-600 dark:text-slate-200">{dateCreated}</p>
+
+          {user?.displayName === author && (
+            <div className="flex gap-2">
+              <Link
+                to={`/posts/edit-post/${id}`}
+                className="flex items-center gap-2 justify-center w-fit bg-blue-500 p-2"
+              >
+                <FaPen /> Edit
+              </Link>
+              <button
+                onClick={() => {
+                  deletePost(id);
+                  navigate(0);
+                }}
+                className="flex items-center gap-2 justify-center w-fit bg-red-500 p-2"
+              >
+                <FaTrash /> Delete
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </motion.div>
