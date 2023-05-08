7912fc234d32bed96e09aa3b879faf7243f53c61
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 02:09:12 +0000

    Add newsletter section

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,11 @@
-import { Container, Header, PostsContainer } from "./components";
+import { Header, Newsletter, PostsContainer } from "./components";
 
 function App() {
   return (
     <>
       <Header />
-
       <PostsContainer />
-      <Container>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          eligendi enim dolores atque deleniti nulla commodi. Rerum quasi eaque
-          reprehenderit velit obcaecati esse, doloremque accusantium tempore,
-          numquam alias iste maiores placeat excepturi consectetur deleniti!
-          Maxime harum ad fugit eveniet debitis maiores, repellat velit
-          provident ut eos? Vero alias sed aut rem, repellat quibusdam molestiae
-          neque nisi omnis, officiis iusto totam modi ratione qui sint,
-          molestias obcaecati perferendis nemo explicabo magni praesentium!
-          Quasi explicabo nemo, amet minima soluta dolorum! Distinctio debitis
-          reprehenderit culpa commodi facilis! Veritatis modi sit fugit culpa
-          laborum reprehenderit distinctio quod sequi quos autem. Quo libero
-          quas fuga nulla magni ab quod reiciendis quia iure voluptatum tempora,
-          blanditiis adipisci dolores? Ea aliquam quidem delectus eos, obcaecati
-          molestias fugiat dignissimos in fuga quasi, ducimus, harum tempore
-          dolorum odio accusantium laborum fugit hic voluptatibus placeat
-          maiores. Ipsa quis quae itaque necessitatibus minima distinctio veniam
-          doloribus ratione at perferendis facere, quam aut aspernatur similique
-          sint nostrum deleniti, esse fuga dolore fugit vitae. Corrupti esse rem
-          maxime laudantium quasi eius sequi tempore quas! Quas soluta vero
-          delectus et. Cum non quis eligendi magnam in! Inventore fugit natus
-          animi, repellendus nulla ratione debitis reiciendis neque accusamus
-          impedit officia in blanditiis sequi nemo explicabo magni architecto,
-          voluptates laborum quo ullam voluptas. Ducimus, blanditiis accusamus
-          quibusdam earum ratione asperiores nostrum laborum eveniet quaerat
-          obcaecati aperiam alias autem dolorum quae commodi distinctio,
-          corporis beatae itaque dolores odio suscipit magni. Magnam fugiat
-          necessitatibus incidunt excepturi hic dicta vitae sed facere deserunt
-          ipsam, impedit provident nobis a. Praesentium voluptatum ullam,
-          nesciunt blanditiis eum sint impedit enim suscipit beatae alias
-          obcaecati distinctio repellat. Nisi quam rem beatae? Molestias a
-          minima earum recusandae debitis magni voluptate exercitationem velit
-          consequatur, molestiae placeat et temporibus similique dolores tenetur
-          eveniet, error nostrum suscipit nesciunt cupiditate iste
-          necessitatibus illum? Quasi assumenda consequuntur voluptatibus ipsa
-          similique rem perferendis laborum, tempore reiciendis, repudiandae vel
-          excepturi dicta deleniti est architecto veritatis. Quis, consequuntur!
-          Repellat quod minima repellendus odit a nisi labore doloribus rem
-          velit. Rem, quam tenetur nesciunt aperiam illo nostrum blanditiis
-          mollitia doloribus, impedit necessitatibus aliquam, obcaecati a magni
-          commodi neque officiis quidem dicta! Vero neque iusto inventore eos
-          reprehenderit libero velit, veniam dolores tenetur sed maxime in.
-          Optio, consequatur! Suscipit, eligendi obcaecati! Tempore quaerat
-          doloribus, dignissimos nisi, vero dicta provident rem accusantium
-          quisquam accusamus ipsum, harum soluta laborum nihil maxime aliquid
-          ratione nobis incidunt. Cumque voluptate unde illo dolor nihil illum
-          impedit sed dicta eligendi architecto accusantium in odit est
-          provident repellat doloribus dolore, voluptas omnis tenetur deserunt
-          eius aliquam esse natus. Mollitia labore quaerat corrupti voluptate
-          consequatur, voluptas dolores debitis eius accusamus, voluptatem
-          similique vero molestias quod! Eaque quo autem rem distinctio
-          reiciendis, dolores totam quis. Aspernatur sint temporibus possimus
-          dolores, itaque porro totam a minima, repellendus corporis dolorum
-          laboriosam unde, aperiam amet! Doloribus saepe nam quis corporis
-          ducimus blanditiis quo! Voluptatibus, repellendus quos. Suscipit nobis
-          at est, neque reiciendis nulla explicabo, mollitia minima omnis natus
-          iste magnam recusandae doloremque, ad numquam accusamus dolorem. Quas
-          doloribus ad voluptatum iure, accusantium reiciendis facilis fugiat
-          quaerat?
-        </p>
-      </Container>
+      <Newsletter />
     </>
   );
 }
