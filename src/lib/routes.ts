ad905ac0c65650d2e2c6ae603727b0c1da230a57
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 02:02:59 +0000

    add route icons

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,8 +1,23 @@
+import { FaNewspaper, FaPenFancy, FaSignInAlt } from "react-icons/fa";
 import { routeT } from "../types";
 
 export const ROUTES: routeT[] = [
-  { name: "Blog Posts", href: "/posts" },
-  { name: "Create Post", href: "/create-post" },
-  { name: "Contact", href: "/contact" },
-  { name: "Newsletter", href: "/newsletter" },
+  {
+    name: "Blog Posts",
+    href: "/posts",
+    protectedRoute: false,
+    icon: FaNewspaper({ size: 20 }),
+  },
+  {
+    name: "Create Post",
+    href: "/create-post",
+    protectedRoute: true,
+    icon: FaPenFancy({ size: 20 }),
+  },
+  {
+    name: "Login",
+    href: "/login",
+    protectedRoute: false,
+    icon: FaSignInAlt({ size: 20 }),
+  },
 ];
