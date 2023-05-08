34737a530624147f3425951cc0abb61ea732f1ab
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 01:46:37 +0000

    add route for editing post and a toast container to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import "./index.css";
-import { AddPost, Login, Posts } from "./pages";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreatePost, EditPost, Login, Post, Posts } from "./pages";
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />} />
         <Route path="/posts" element={<Posts />} />
-        <Route path="/add-post" element={<AddPost />} />
+        <Route path="/posts/:title" element={<Post />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/posts/edit-post/:id" element={<EditPost />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
