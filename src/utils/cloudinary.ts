893eafd185f2b187009921b23df4aaaf8c6acaee
William M. Tsikata <tsikatawill@gmail.com>
2023-05-08 02:04:49 +0000

    use url variable inplace of hardcoded value

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -6,12 +6,7 @@ export const uploadImage = async (formData: FormData) => {
   }/image/upload?upload_preset=neqfirer`;
 
   const imageUrl = await axios
-    .post(
-      `https://api.cloudinary.com/v1_1/${
-        import.meta.env.VITE_cloudId
-      }/image/upload`,
-      formData
-    )
+    .post(url, formData)
     .then((res) => res.data.url)
     .catch((err) => ({ uploadError: err }));
 
